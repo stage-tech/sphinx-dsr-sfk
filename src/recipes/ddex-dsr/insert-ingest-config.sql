50344767ab3f801f159d990d8e0336c4e616959d
laiskr <laiskr@telesoftas.com>
2021-10-22 14:53:12 +0300

    feat: add amazon_online BAP 1.2 ingest config

--- src/recipes/ddex-dsr/insert-ingest-config.sql
+++ src/recipes/ddex-dsr/insert-ingest-config.sql
@@ -6,6 +6,7 @@ insert into ingest_config (SOURCE, SCHEMA_VERSION, SCHEMA, FUNCTION)
 values
 ('spotify_online', 'BasicAudioProfile:1.2', 'UNTYPED_BAP_MRB_12', 'STAGING.DDEX_DSR_PARSER_YOUTUBE'),
 ('amazon_online', 'BasicAudioProfile:1.1', 'UNTYPED_BAP_MRB_11', 'STAGING.DDEX_DSR_PARSER_AMAZON'),
+('amazon_online', 'BasicAudioProfile:1.2', 'UNTYPED_BAP_MRB_12', 'STAGING.DDEX_DSR_PARSER_AMAZON'),
 ('amazon_online', 'BasicAudioProfile:1.3', 'UNTYPED_BAP_MRB_13', 'STAGING.DDEX_DSR_PARSER_AMAZON'),
 ('youtube_online', 'AudioVisualProfile:1.0', 'UNTYPED_AVP_MRB_10', 'STAGING.DDEX_DSR_PARSER_YOUTUBE'),
 ('tiktok_online', 'BasicAudioProfile:1.2', 'UNTYPED_BAP_MRB_12', 'STAGING.DDEX_DSR_PARSER_YOUTUBE');
